c9ddf7b2673ff0734b12e72ff579066a1d1f1c67
Redinaldo Gomes <redi.gomes@gmail.com>
2019-11-30 12:12:55 -0300

    Final web app hook test

--- src/App.js
+++ src/App.js
@@ -10,10 +10,28 @@ export default function App() {
     setRepositories(data);
   }, []);
 
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favorites`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
     <ul>
       {repositories.map(repo => (
-        <li key={repo.id}>{repo.name}</li>
+        <li key={repo.id}>
+          {repo.name}<> </>
+          {repo.favorite && <span>(Favorito) </span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
       ))}
     </ul>
   );
